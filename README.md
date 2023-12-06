942abb31bb1c03a901ac52ad3343f8559d49a2d7
Jason Shui <jiashui@hotmail.com>
2023-12-06 16:22:07 +0800

    add ros scripts
    Signed-off-by: Jason Shui <jiashui@hotmail.com>

--- README.md
+++ README.md
@@ -1,8 +1,6 @@
 # shuienv
 
-## Debian/Ubuntu Only
-
-Tested on Ubuntu 22.04
+Debian/Ubuntu Only, tested on Ubuntu 22.04
 ```
 bash -c "$(wget -O - https://raw.githubusercontent.com/JasonShui716/shuienv/main/shuienv)"
 ```
--- set_alias.bash
+++ set_alias.bash
@@ -1,2 +1,17 @@
 #!/bin/bash
+
 echo 'alias cmake-clang="cmake -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_EXPORT_COMPILE_COMMANDS=ON"' >> $HOME/.bashrc
+
+echo 'alias docker-run="docker run -it \
+    -u $(id -u):$(id -g) \
+    --network=host \
+    -e "QT_X11_NO_MITSHM=1" \
+    -e GDK_SCALE \
+    -e GDK_DPI_SCALE \
+    -e DISPLAY=unix$DISPLAY \
+    --restart=unless-stopped \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    --privileged --security-opt seccomp=unconfined \
+    --env="DISPLAY" \
+    -v /tmp/.X11-unix:/tmp/.X11-unix"' >> $HOME/.bashrc
+
