8d85129b486026f0383ad204d9dd09b5808b28dd
Jason Shui <jiashui@hotmail.com>
2023-12-06 15:07:54 +0800

    add cmake
    Signed-off-by: Jason Shui <jiashui@hotmail.com>

--- install_env.bash
+++ install_env.bash
@@ -2,6 +2,8 @@
 set -x
 set -e
 
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
 pushd ~
 mkdir -p workspace
 mkdir -p playground
@@ -51,23 +53,34 @@ sudo rm -rf /squashfs-root
 sudo mv squashfs-root /
 sudo ln -sf /squashfs-root/AppRun /usr/bin/nvim
 LV_BRANCH='release-1.3/neovim-0.9' bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/release-1.3/neovim-0.9/utils/installer/install.sh)
-export PATH=$PATH:$HOME/.local/bin
+echo 'export PATH=$PATH:$HOME/.local/bin' > $HOME/.bashrc
+
 mkdir font && pushd font
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/JetBrainsMono.tar.xz
 tar xvf Jet*
 sudo cp -r *ttf /usr/share/fonts/
 sudo fc-cache -fv
+popd # font
 
-# install clang
+# install cmake, clang
 sudo bash -c 'touch /etc/apt/apt.conf.d/99verify-peer.conf \
 && echo >>/etc/apt/apt.conf.d/99verify-peer.conf "Acquire { https::Verify-Peer false }"'
+
+wget https://github.com/Kitware/CMake/releases/download/v3.27.9/cmake-3.27.9-linux-x86_64.sh
+sudo bash cmake*.sh  --skip-license --prefix=/usr/local
+
 sudo bash -c "$(wget -O - https://apt.llvm.org/llvm.sh)"
-sudo apt install -y libc++abi-17-dev libc++-17-dev lldb-17 clangd-17
+sudo apt install -y libc++abi-17-dev libc++-17-dev lldb-17 clangd-17 libstdc++-13
 sudo update-alternatives --install /usr/bin/clang clang /usr/bin/clang-17 100
 sudo update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang++-17 100
 sudo update-alternatives --install /usr/bin/lldb lldb /usr/bin/lldb-17 100
 sudo update-alternatives --install /usr/bin/clangd clangd /usr/bin/clangd-17 100
 clang -v
 clang++ -v
+
+popd # install env
+popd # home
+
+bash $SCRIPT_DIR/set_alias.bash
 
 cd ~ && rm -rf ~/install_env
---
+++ set_alias.bash
@@ -0,0 +1,2 @@
+#!/bin/bash
+echo 'alias cmake-clang="cmake -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_EXPORT_COMPILE_COMMANDS=ON"' >> $HOME/.bashrc
